521549d0210ce1be9c24d4e05d1fdb297475eb0a
Mariamol James <mariamol.james@gmail.com>
2020-06-04 19:57:50 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,12 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
+  newstring=""
   if string[0] == "s"
-    10 * "s" + string
+    10.times do
+      newstring+="s"
+    end
+    newstring + string
   else
     string
   end
